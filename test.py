e3c4f2078b195e6be67805f0e6b477547f182d44
Karthikrm2607 <karthikrm2607@gmail.com>
2024-07-24 17:11:33 +0530

    deduplication and union added

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import os
+from pyspark.sql import SparkSession
 
-help(os)
+spark = SparkSession.builder.appName("test").getOrCreate()
+
+df = spark.read.format("csv").option("header","true").option("inferSchema","true").load("E:\\0_BIG DATA\\PROJECTS\\iNeuron_Karthik\\A_RAW_DATA\B_DELTA_DATA\\address.csv\\11.2.Delta_Address.csv")
+print(df.columns)
+for col in df.columns:
+    df = df.withColumnRenamed(col, col.lower())
+
+df.show()
